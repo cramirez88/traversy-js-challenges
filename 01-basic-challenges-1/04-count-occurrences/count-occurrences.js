9680d2017b570ff444e09b38329e77fee3959d30
Christian Ramirez <christian.r0602@gmail.com>
2023-10-31 21:22:10 -0400

    count occurences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,13 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let totalOccurences = 0
+  for(let i = 0; i < str.length; i++){
+    
+    if(str[i] === char){
+      totalOccurences++
+    }
+  }
+  return totalOccurences
+}
+
 
 module.exports = countOccurrences;
