33dced4e5cfcad1d849dd961743de38a564af72d
Christian Ramirez <christian.r0602@gmail.com>
2023-11-01 11:31:24 -0400

    title case algo

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,13 @@
-function titleCase() {}
+function titleCase(str) {
+  // split method will put the string into array at every space
+  let splittingStr = str.toLowerCase().split(' ')
+  for(let i = 0; i < splittingStr.length; i++){
+    // loop through array first letter
+    console.log(splittingStr[i][0].toUpperCase())
+    splittingStr[i] = splittingStr[i][0].toUpperCase() + splittingStr[i].slice(1)
+    splittingStr
+  }
+  return splittingStr.join(' ')
+}
 
 module.exports = titleCase;
