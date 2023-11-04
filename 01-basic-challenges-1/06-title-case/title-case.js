d979f98165b01b6ee1ffdfba946f0aa9dd7c9c2a
Christian Ramirez <christian.r0602@gmail.com>
2023-11-04 12:44:58 -0400

    title case redo

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,13 +1,16 @@
 function titleCase(str) {
-  // split method will put the string into array at every space
-  let splittingStr = str.toLowerCase().split(' ')
-  for(let i = 0; i < splittingStr.length; i++){
-    // loop through array first letter
-    console.log(splittingStr[i][0].toUpperCase())
-    splittingStr[i] = splittingStr[i][0].toUpperCase() + splittingStr[i].slice(1)
-    splittingStr
-  }
-  return splittingStr.join(' ')
+//  returns the string with the first letter of the word capitalized
+// what data structure allows me to do this more easily?-> Arrays
+// convert string to array
+let stringArray = str.split(' ')
+// loop through array and with the first character of each item capitalize the letter
+for (let i = 0; i < stringArray.length; i++){
+  // console.log(stringArray[i][0].toUpperCase())
+  // returns the first letter capitalized --> I need to tack on the rest of the string
+  console.log(stringArray[i][0].toUpperCase() + stringArray[i].slice(1))
+  stringArray[i]= stringArray[i][0].toUpperCase() + stringArray[i].slice(1)
+}
+return stringArray.join(' ')
 }
 
 module.exports = titleCase;
