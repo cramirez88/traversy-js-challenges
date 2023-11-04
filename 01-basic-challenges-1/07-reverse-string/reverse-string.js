ed2c1718dde0e3d11c61d20a8637f4b4f66fcddb
Christian Ramirez <christian.r0602@gmail.com>
2023-11-04 13:09:20 -0400

    reverse strimg

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+function reverseString(str) {
+  // takes in a string and return the reverse of that string
+let reverse = ''
+
+for(let i = str.length - 1; i >= 0; i--){
+  reverse += str[i]
+}
+return reverse
+}
 
 module.exports = reverseString;
