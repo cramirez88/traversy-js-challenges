907e42d212cebc01afc1a3aa006692057bdd1a85
Christian Ramirez <christian.r0602@gmail.com>
2023-11-13 21:19:32 -0500

    isPalindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,13 +1,38 @@
 function isPalindrome(str) {
-  let j = str.length - 1
- for(let i = 0; i < str.length/2; i++){
-  if(str[i] !== str[j]){
-    return false
-  }
-  j--
- }
- return true
+  let first = removeNonAlphaNumeric(str.toLowerCase())
+  let second = comparativeString(first)
+  return first === second
 }
+
+
+// function that returns a string with only alphanumeric characters
+function removeNonAlphaNumeric(str){
+  let nonAlphaString = ''
+  for(let i = 0; i < str.length; i++){
+    const letter = str[i]
+    if(isAlphaNumeric(letter)){
+      nonAlphaString += letter
+    }
+    }
+    return nonAlphaString
+  }
+
+function isAlphaNumeric(char){
+  let code = char.charCodeAt(0)
+  return (
+    code >=48 && code <= 57 || code >= 97 && code <= 122
+  )
+}
+
+function comparativeString(str){
+  let compString = ''
+  for(let i = str.length - 1; i >= 0; i--){
+    compString += str[i]
+    console.log('Comparative String is: ' + compString)
+  }
+  return compString
+}
+
 
 
 
