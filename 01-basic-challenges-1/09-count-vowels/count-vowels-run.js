d9c8bd28933481d998b1b50dee8e1a95420bdb1e
Christian Ramirez <christian.r0602@gmail.com>
2023-11-14 18:01:11 -0500

    remove duplicates algo

--- 01-basic-challenges-1/09-count-vowels/count-vowels-run.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels-run.js
@@ -1,5 +1,5 @@
 const countVowels = require('./count-vowels');
 
-const result = countVowels('OpenAI Chatbot');
+const result = countVowels('Hello World!');
 
 console.log(result);
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,11 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  let newArray = []
+  for (let i = 0; i < arr.length; i++){
+    if(!newArray.includes(arr[i])){
+      newArray.push(arr[i])
+    }
+  }
+  return newArray
+}
 
 module.exports = removeDuplicates;
