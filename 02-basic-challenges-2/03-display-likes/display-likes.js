b4868c1820730ce114c070fc3ed560bae5fa7949
Christian Ramirez <christian.r0602@gmail.com>
2023-11-24 12:49:21 -0500

    display likes alg

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,19 @@
-function displayLikes(nameArray) {}
+function displayLikes(nameArray) {
+  // based on the array given, the function should return the names in the array as the people who liked the post
+  
+  for(let i = 0; i <= nameArray.length; i++){
+    if(nameArray.length === 0){
+      return 'no one likes this'
+    }else if(nameArray.length === 1){
+      return `${nameArray[0]} likes this`
+    }else if(nameArray.length === 2){
+      return `${nameArray[0]} and ${nameArray[1]} like this`
+    }else if(nameArray.length === 3){
+      return `${nameArray[0]}, ${nameArray[1]} and ${nameArray[2]} like this`
+    }else {
+      return `${nameArray[0]}, ${nameArray[1]} and ${nameArray.length - 2} others like this`
+    }
+  }
+}
 
 module.exports = displayLikes;
