959ca412148deaba05b363814216fa060cef1a01
Christian Ramirez <christian.r0602@gmail.com>
2023-11-24 13:29:15 -0500

    find the missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,20 @@
-function findMissingNumber() {}
+function findMissingNumber(numArr) {
+  if(numArr === []) {
+    return 1
+  }
+  if(!numArr){
+    return undefined
+  }
+
+  let lengthOfArray = numArr.length + 1
+
+  let realSum = lengthOfArray * (lengthOfArray + 1)/2
+  let actualSum = 0
+  for(let i = 0; i < numArr.length; i++){
+    actualSum += numArr[i]
+  }
+
+  return realSum - actualSum
+}
 
 module.exports = findMissingNumber;
