845f0305502049aab71e7daa3437433da80a1b79
Christian Ramirez <christian.r0602@gmail.com>
2024-01-27 11:37:55 -0600

    count occurences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,12 +1,12 @@
 function countOccurrences(str, char) {
-  let totalOccurences = 0
+  // you're given a string and you want to loop through it, and as you go, keep track of occurences of such character.
+  let charOccurrences = 0
   for(let i = 0; i < str.length; i++){
-    
-    if(str[i] === char){
-      totalOccurences++
+    if(char === str[i]){
+      charOccurrences++
     }
   }
-  return totalOccurences
+  return charOccurrences
 }
 
 
